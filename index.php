3ef07a38ac159f290824a729cf0e9618cc01701f
github <i.koutivas@gmail.com>
2019-06-05 16:35:19 +0300

    added favicon

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='icon' href='favicon.ico' type='image/x-icon'/ >
     <link rel="stylesheet" href="style.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900&display=swap" rel="stylesheet">
     <title>Ποινικός Κώδικας</title>
